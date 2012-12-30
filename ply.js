b84e354d5c55f8da160bdccf9935ed2a564fcc78
Steven Lu <stevenlu443@gmail.com>
2012-12-29 19:26:23 -0500

    looking pretty clean so far

--- ply.js
+++ ply.js
@@ -1,1 +1,42 @@
-// I don't see the point of jquery (function($){})(jQuery) wrapper
+//////////////////////////////
+/////////// ply.js ///////////
+//////////////////////////////
+// This is a jQuery library //
+//////////////////////////////
+
+// When using the library, please be careful about stopPropagation(). It will prevent things from functioning. 
+
+var PLY = (function($){
+	// contains all "global" state of the library. 
+	var data_model = {
+		keys_depressed: {}, 
+		// the (serialized HTML) debug view of the data-model (i.e., me) 
+		dump: function () {
+			var str = "<ul>";
+			for (var prop in this) {
+				str += "<li>";
+				str += prop + ": "; 
+				str += this[prop].toString();
+				str += "</li>";
+			}
+			str += "</ul>";
+		}
+	};
+	// entry point for code is the document's event handlers. 
+	var handlers_for_doc = {
+		mousedown: function(evt) {
+
+		},
+		mouseup: function(evt) {
+
+		},
+		mousemove: function(evt) {
+
+		}
+	};
+	// for the sake of simplicity i rely on jQuery to correctly bind event handlers
+	for (var event_name in handlers_for_doc) {
+		$(document).on(event_name, handlers_for_doc[event_name]);
+	}
+	return data_model;
+})(jQuery);
