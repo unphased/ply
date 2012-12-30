e4c30d78d2884703e77cc1bf7b42c70194eadefc
Steven Lu <stevenlu443@gmail.com>
2012-12-30 01:48:58 -0500

    some mouse handling

--- ply.js
+++ ply.js
@@ -36,7 +36,15 @@ var PLY = (function($) {
 		// Never assume that keys is not filled with keys that were held down 
 		// the last time the browser was in focus.		
 		keys_depressed: {}, 
-		mouse_state: {}
+
+		// The pointer_state array's order is important. It is maintained 
+		// the order that touches are created. If you lift a finger it is 
+		// removed and the rest of the array's ordering remains. 
+		// all mouse-pointer activity is mapped onto the first pointer. This is 
+		// not terribly complicated because on a mouse system in all likelihood
+		// (barring strange IE10 touch-notebook situations) the mouse will be 
+		// the only member of the pointer list. 
+		pointer_state: []
 	};
 
 	function key(evt) {
@@ -46,13 +54,30 @@ var PLY = (function($) {
 	// entry point for code is the document's event handlers. 
 	var handlers_for_doc = {
 		mousedown: function(evt) {
+			// need to trap drag-of-selection. Crap. Will just have to use 
+			// an ugly work-around in the mouseup. Funny this stuff is quite
+			// less problematic for touch events. 
 
+			// trap the right clicks!! this is huge
+			if (evt.which === 3) // secondary mouse button causes context menu
+				// context menu prevents mouseup. ply by default ignores
+				// the secondary mouse button interaction
+				return;
+			var x = {ty:"m", x:evt.clientX, y:evt.clientY};
+			exposed.pointer_state.unshift(x); // keep at front
 		},
 		mouseup: function(evt) {
-
+			// this event may fail to fire by dragging mouse out of
+			// window. This is less of a concern for touch since most touch
+			// devices do not use window systems. 
+			for (var i=0;i<exposed.pointer_state.length;++i) {
+				if (exposed.pointer_state[i].ty === "m") {
+					exposed.pointer_state.splice(i,1); --i; 
+				}
+			}
 		},
 		mousemove: function(evt) {
-
+			
 		}, 
 		keydown: function(evt) {
 			exposed.keys_depressed[key(evt)] = String.fromCharCode(key(evt));
