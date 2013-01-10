2c704e08d279b54a454b73784a8c7a0ad2b40400
Steven Lu <stevenlu443@gmail.com>
2013-01-09 21:10:59 -0500

    okay fixing that here too

--- ply.js
+++ ply.js
@@ -29,7 +29,7 @@
 var PLY = (function ($) {
     
     // all vars except the variable "exposed" are private variables 
-    var git_context = "#%REVISION%#",
+    var git_context = "#%REVISION%#";
 
     // various parts of state of the library 
     // accessible via window.PLY to allow debug display
