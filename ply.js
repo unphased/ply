98c672fadbf5064480f361f8b54db2a338d9956c
Steven Lu <stevenlu443@gmail.com>
2013-01-12 21:58:52 -0500

    script writes git hash into js file

--- ply.js
+++ ply.js
@@ -31,7 +31,7 @@ var PLY = (function ($) {
     // all vars except the variable "exposed" are private variables 
     var log_buffer = [];
 
-    var git_context = "#% 714a768 should address the other issue %#";
+    var git_context = "#% 93d9c5d still looking %#";
 
     // various parts of state of the library 
     // accessible via window.PLY to allow debug display
