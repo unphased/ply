f7013a6bee7d7ffe63ca6246401e5f57bcb737d8
Steven Lu <stevenlu443@gmail.com>
2013-02-05 00:55:07 -0500

    sexy script autoloading

--- dynamic_load_bookmark.js
+++ dynamic_load_bookmark.js
@@ -1,19 +1,20 @@
 (function(){
 	// serial script loading 
-	var jq=document.body.appendChild(document.createElement('script'));
-	jq.src='http://code.jquery.com/jquery-1.8.3.min.js';
-	jq.onload=function(){
-		var ply=document.body.appendChild(document.createElement('script'));
-		ply.src='http://unphased.github.com/ply/ply.js';
-		ply.onload=function(){
-			var dbg=document.body.appendChild(document.createElement('script'));
-			ply.src='http://unphased.github.com/ply/debug.js';
-			ply.onload=function(){
+	function load(url,cb){var x=document.body.appendChild(document.createElement('script'));x.src=url;if (cb){x.onload=cb;}}
+	load('http://code.jquery.com/jquery-1.8.3.min.js',function(){
+		load('http://unphased.github.com/ply/ply.js',function(){
+			load('http://unphased.github.com/ply/2ply.js');
+			load('http://unphased.github.com/ply/debug.js',function(){
 				// some basic UI to allow selection of features via my debug lib
 				// In order to preserve regular site functionality as much as possible, 
 				// a double-tap custom gesture is required to start selection mode. 
 				
-			}
-		};
-	};
+				PLY.attach_handlers_on_document({
+					touchstart: function() {},
+					touchmove: function() {},
+					touchend: function() {}
+				});
+			});
+		});
+	});
 })();
