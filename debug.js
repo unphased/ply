dc50ae6bc5206e5b00ece3aa0ff1f843593ab783
Steven Lu (desktop i5-2500k) <stevenlu443@gmail.com>
2013-02-06 00:28:24 -0500

    scripted update of code from master at ac39951 try this now

--- debug.js
+++ debug.js
@@ -68,7 +68,7 @@ var DEBUG = (function() {
     // all vars except the variable "exposed" are private variables 
     var log_buffer = [];
 
-    var git_context = "#%  %#";
+    var git_context = "#% ac39951 try this now %#";
 
     var datenow = Date.now?Date.now:function(){return (new Date()).getTime();};
 
