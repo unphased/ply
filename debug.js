5ae379d4c13cad2ef581c7ab2935f5c84fc1e58e
Steven Lu (desktop i5-2500k) <stevenlu443@gmail.com>
2013-02-06 00:50:29 -0500

    a bit more progress

--- debug.js
+++ debug.js
@@ -128,15 +128,16 @@ var DEBUG = (function() {
         var target = jc.children(selector);
         if (!e) { // remove command: remove if present
             target.remove();
-        } else if (target.length === 0) { // update command: add if not present
-            jc.append("<div "+(identifier?"data-id="+identifier:"id=debug_element_highlighter_noid")+"></div>");
-            target = jc.children(selector);
-            target.css({ // init
+        } else {
+            if (target.length === 0) { // update command: add if not present
+                jc.append("<div "+(identifier?"data-id="+identifier:"id=debug_element_highlighter_noid")+"></div>");
+                target = jc.children(selector);
+                target.css({ // init
 
-            });
+                });
+            }
+            // assign to the target styles that has it overlap the target element
         }
-        // assign to the target styles that has it overlap the target element
-
     }
 
     // primitive set of methods provided by debug
