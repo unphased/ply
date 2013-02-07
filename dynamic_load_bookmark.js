c9117bb475ed98031955a763a023c6b9f589f673
Steven Lu <stevenlu443@gmail.com>
2013-02-07 11:32:39 -0500

    race condition

--- dynamic_load_bookmark.js
+++ dynamic_load_bookmark.js
@@ -2,9 +2,10 @@
     // serial script loading 
     function load(url,cb){var x=document.body.appendChild(document.createElement('script'));x.src=url;if (cb){x.onload=cb;}}
     load('http://code.jquery.com/jquery-1.8.3.min.js',function(){
-        load('http://unphased.github.com/ply/debug.js',function(){
-            load('http://unphased.github.com/ply/2ply.js'); // 2ply can be executed at any point to transparently augment functionality
+        load('http://unphased.github.com/ply/debug.js',function(){            
             load('http://unphased.github.com/ply/ply.js',function(){
+                load('http://unphased.github.com/ply/2ply.js'); // 2ply can be executed at any point to transparently augment functionality                
+
                 // some basic UI to allow selection of features via my debug lib
                 // In order to preserve regular site functionality as much as possible, 
                 // a double-tap custom gesture is required to start selection mode.
