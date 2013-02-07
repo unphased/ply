9887129378a16de47e2441582487854a11ae33e4
Steven Lu <stevenlu443@gmail.com>
2013-02-07 15:51:33 -0500

    need revision

--- debug.js
+++ debug.js
@@ -232,6 +232,7 @@ var DEBUG = (function() {
     // generally helpful debugging info
     console.log("UA: "+navigator.userAgent);
     console.log("window.devicePixelRatio:", window.devicePixelRatio);
+    console.log("Revision: "+DEBUG.revision);
 
     return exposed;
 })();
