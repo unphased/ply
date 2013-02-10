0dd76c68e0bfc9e49c41db6a170cf7eb1e5f25ec
Steven Lu <stevenlu443@gmail.com>
2013-02-10 14:32:09 -0500

    a cubic bezier

--- debug.js
+++ debug.js
@@ -155,7 +155,7 @@ var DEBUG = (function() {
         hyphen_mp('transitionDuration') + ": 0.2s, 0.2s; \n\t" + 
         hyphen_mp('transitionProperty') + ": "+hyphen_mp('transform')+", opacity; \n\t" +
         hyphen_mp('transformOrigin') + ": 0 0; \n\t" + 
-        hyphen_mp('transitionTimingFunction') + ": ease-out; \n" +
+        hyphen_mp('transitionTimingFunction') + ": cubic-bezier(0.500, 0.500, 0.500, 1.000); \n" +
         "\tposition: absolute; top: 0; left: 0; \n" + 
         "\tpointer-events: none; height: 500px; width: 500px; \n} \n" + 
         "#debug_element_highlighter_outer {\n\tbackground-color: rgba(45,60,255,0.2); \n} \n" + 
