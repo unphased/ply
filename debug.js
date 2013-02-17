119beed82abaaa3318e3934e2be1a67e6dfb8d2a
Steven Lu <stevenlu443@gmail.com>
2013-02-16 20:37:17 -0500

    0.2~0.4 now and also much slower

--- debug.js
+++ debug.js
@@ -172,14 +172,14 @@ var DEBUG = (function() {
         "\tpointer-events: none; height: 500px; width: 500px; \n} \n" + 
         "#debug_element_highlighter_outer {\n\tbackground-color: rgba(45,60,255,0.2); \n} \n" + 
         "#debug_element_highlighter_inner {\n\tbackground-color: rgba(25,255,35,0.2); \n} \n" + 
-        "@"+keyframesPrefixed+" pulsate_opacity_light {\n\tfrom {\n\t\topacity: 0;\n\t}\n\tto {\n\t\topacity: 0.4;\n\t}\n}\n" + 
+        "@"+keyframesPrefixed+" pulsate_opacity_light {\n\tfrom {\n\t\topacity: 0.2;\n\t}\n\tto {\n\t\topacity: 0.4;\n\t}\n}\n" + 
         "#debug_element_focused {\n\t" + 
         "background-color: orange;\n\t" + 
         hyphen_mp('animationName') + ": pulsate_opacity_light;\n\t" + 
         hyphen_mp('animationIterationCount') + ": infinite;\n\t" + 
         hyphen_mp('animationDirection') + ": alternate;\n\t" + 
         hyphen_mp('animationTimingFunction') + ": ease;\n\t" + 
-        hyphen_mp('animationDuration') + ": 0.5s;\n" + 
+        hyphen_mp('animationDuration') + ": 1.1s;\n" + 
         "} \n";
 
     // append a style tag to head 
