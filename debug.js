b3cfc204b93245b239c80a5d9b9e2ab8ff6babef
Steven Lu <stevenlu443@gmail.com>
2013-02-19 03:15:18 -0500

    a CSS capitalization [Webkit or webkit]

--- debug.js
+++ debug.js
@@ -352,10 +352,10 @@ var DEBUG = (function() {
             console.log("opacity_now before removing class",opacity_now);
             // set it so it does not flicker as animation terminates
             focus.style[transitionDurationStyle] = "0";
-            console.log("a ",getComputedStyle(focus).getPropertyValue(transitionDurationStyle));
+            console.log("a ",getComputedStyle(focus).getPropertyValue(transitionDurationStyle.replace('Webkit','webkit')));
             focus.style.opacity = opacity_now; 
             jfocus.removeClass('pulsate_opacity'); // cause animation to terminate
-            console.log("b ",getComputedStyle(focus).getPropertyValue(transitionDurationStyle));
+            console.log("b ",getComputedStyle(focus).getPropertyValue(transitionDurationStyle.replace('Webkit','webkit')));
             focus.style[transitionDurationStyle] = ""; 
             console.log("c ", window.getComputedStyle(focus).getPropertyValue('opacity'));
             focus.style.opacity = "0"; // fade out
