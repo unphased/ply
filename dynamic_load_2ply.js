8c2a8e8b2d6d4743c5d808616a57d08b2ad1abb3
Steven Lu <stevenlu443@gmail.com>
2013-02-20 14:50:52 -0500

    a tiny comment change

--- dynamic_load_2ply.js
+++ dynamic_load_2ply.js
@@ -68,7 +68,7 @@
                     },
                     mouseup: function(evt) { console.log("mouseup");
                         if (select_active) {
-                            if (enable_ctx_menu && evt.which === 3) { // if we've not moved outside 
+                            if (enable_ctx_menu && evt.which === 3) { // if we're about to trigger ctxmenu
                                 // do not go on to select, just abort the action
                                 select_active = false;
                                 DEBUG.highlight(null);
