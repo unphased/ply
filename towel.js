c4e5e60e45a184f31cadee87e835342be4636ac4
Steven Lu <stevenlu443@gmail.com>
2013-03-24 14:25:05 -0400

    did some cleanup and added a synchronous js loader

--- towel.js
+++ towel.js
@@ -4,20 +4,23 @@
 
 // towel.js contains a set of utilities. It is for keeping things DRY. 
 
-var TOWEL = (function () {
+var UTIL = (function () {
     "use strict";
     
     // parallel script loading, could perhaps be using jQuery deferred/promises
     // but I *really* like the elegant simplicity of my approach here
     // can put in potentially null entries in array (they will be cleanly skipped)
-    // Sample usage: 
+    // Sample (not very dry example) usage: 
 
-    // var resources = [
+    // resources = [
     //     window.jQuery ? {
     //         url: "https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js",
     //         tag: "script", 
     //         cb: function(){ ply_$ = $.noConflict(true) }
-    //     } : null,
+    //     } : {
+    //         url: "https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js",
+    //         tag: "script"
+    //     },
     //     {url: "https://raw.github.com/unphased/ply/master/modernizr-2.6.2.min.js", tag: "script"}
     // ];
 
@@ -52,37 +55,40 @@ var TOWEL = (function () {
         }
     }
 
-    // named this way because "each" functions generally are for iterating over obj/hashes
-    // but for an array we must ensure the order
-    // cb receives index and item
-    function each_reverse_on_array(array, cb) {
-
+    // synchronous dynamic script loading. I think that the density of awesome in this file is really neat. 
+    // takes an array of js url's to be loaded in that specific order. 
+    function loadjs(resources, cb_done) {
+        var cur_cont = cb_done; // this strange continuation passing procedural programming style is ... strangely fun 
+        // So this is an iterative approach that makes a nested "function stack" where 
+        // the inner functions are hidden inside the closures. 
+        for (var i=resources.length-1; i>=0; --i) {
+            cur_cont = function() {
+                var x = document.body.appendChild(document.createElement('script'));
+                x.src = resources[i];
+                // epic not-quite-recursion. I don't even know what this is called. It's inside-out.
+                x.onload = function() { console.log("loaded "+resources[i]); cur_cont(); }; // TODO: get rid of this function creation once we know it works right 
+            };
+        }
+        cur_cont();
     }
 
-    function loadjs(resources, cb_done) {
-        if (Array.isArray(resources)) {
-            var cur_cont = cb_done; // this strange continuation passing procedural programming style is ... strangely fun 
-            for (var i=resources.length-1; i>=0; --i) {
-                var next_cb = function() {
+    // combined synchronous and asynchronous dynamic script loading scripting sugar.
+    // takes a nested hash (js object) that encodes both the dependency graph
+    // and whatever actions need to be taken during the loading process. Usage example follows
 
-                };
-            }
-        } else {
-            var x=document.body.appendChild(document.createElement('script'));
-            x.src=url;
-            x.onload=function(){
-                console.log("Dynamically loaded "+url);
-                if(cb){cb();}
-            };
-            if(!cb){
-                x.setAttribute('async','')
-            }
-        }
+    // resources = {
+    //     "https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js": {
+    //         tag: "script", 
+    //         cb: function(){ ply_$ = $.noConflict(true) }
+    //     },
+    //     "https://raw.github.com/unphased/ply/master/modernizr-2.6.2.min.js": true
+    // ];
+    function load_js_dependency_tree(resources, cb_all_done) {
+
     }
     
     return {
         each: each,
-        // each_reverse_on_array: each_reverse_on_array,
         async_load: async_load,
         loadjs: loadjs
     };
