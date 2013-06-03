7a93608945e29cba5fdcf15265ebc441f2258bcd
Steven Lu <stevenlu443@gmail.com>
2013-06-03 00:03:16 -0400

    Here is more stuff

--- public/debug.js
+++ public/debug.js
@@ -92,7 +92,7 @@ var DEBUG = (function($) {
             !!('onmsgesturechange' in window); // works on ie10
     }
 
-    var original_console_log = console.log;
+    /* var original_console_log = console.log;
     // echo console logs to the debug
     var instrumented_log = function () {
         original_console_log.apply(window.console, arguments);
@@ -110,21 +110,88 @@ var DEBUG = (function($) {
         // you've got one
     };
 
-    if (true)
-    {
-        console.log = instrumented_log;
+    console.log = instrumented_log; */
 
-        var show_log_buffer = false;
-        $("#log_buffer_dump").before($('<button>toggle full log buffer snapshot</button>').on('click',function(){
-            show_log_buffer = !show_log_buffer;
-            if (show_log_buffer) {
-                $("#log_buffer_dump").html(log_buffer.join(''));
-            } else {
-                $("#log_buffer_dump").html("");
+    // This amazing log wrapper for webkit lifted from http://stackoverflow.com/a/14842659/340947
+    _log = (function (methods, undefined) {
+
+        var Log = Error; // does this do anything?  proper inheritance...?
+        Log.prototype.write = function (args, method) {
+            /// <summary>
+            /// Paulirish-like console.log wrapper.  Includes stack trace via @fredrik SO suggestion (see remarks for sources).
+            /// </summary>
+            /// <param name="args" type="Array">list of details to log, as provided by `arguments`</param>
+            /// <param name="method" type="string">the console method to use:  debug, log, warn, info, error</param>
+            /// <remarks>Includes line numbers by calling Error object -- see
+            /// * http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+            /// * http://stackoverflow.com/questions/13815640/a-proper-wrapper-for-console-log-with-correct-line-number
+            /// * http://stackoverflow.com/a/3806596/1037948
+            /// </remarks>
+
+            // via @fredrik SO trace suggestion; wrapping in special construct so it stands out
+            var suffix = {
+                "@": (this.lineNumber ?
+                    this.fileName + ':' + this.lineNumber + ":1" :
+                    // add arbitrary column value for chrome linking
+                    extractLineNumberFromStack(this.stack)
+                )
+            };
+
+            args = args.concat([suffix]);
+            // via @paulirish console wrapper
+            if (console && console[method]) {
+                if (console[method].apply) { console[method].apply(console, args); } else { console[method](args); } // nicer display in some browsers
             }
-        })).on("touchenter",function(){console.log("touchenter on toggle buffer dump button");})
-            .on('touchleave',function(){console.log("touchleave on toggle buffer dump button");});
-    }
+        };
+        var extractLineNumberFromStack = function (stack) {
+            /// <summary>
+            /// Get the line/filename detail from a Webkit stack trace.  See http://stackoverflow.com/a/3806596/1037948
+            /// </summary>
+            /// <param name="stack" type="String">the stack string</param>
+
+            // correct line number according to how Log().write implemented
+            var line = stack.split('\n')[3];
+            // fix for various display text
+            line = (line.indexOf(' (') >= 0 ?
+                line.split(' (')[1].substring(0, line.length - 1) :
+                line.split('at ')[1]);
+            return line;
+        };
+
+        // method builder
+        var logMethod = function(method) {
+            return function (params) {
+                /// <summary>
+                /// Paulirish-like console.log wrapper
+                /// </summary>
+                /// <param name="params" type="[...]">list your logging parameters</param>
+
+                // only if explicitly true somewhere
+                if (typeof LOGENABLED === typeof undefined || !LOGENABLED) return;
+
+                // call handler extension which provides stack trace
+                Log().write(Array.prototype.slice.call(arguments, 0), method); // turn into proper array & declare method to use
+            };//--  fn  logMethod
+        };
+        var result = logMethod('log'); // base for backwards compatibility, simplicity
+        // add some extra juice
+        for(var i in methods) result[methods[i]] = logMethod(methods[i]);
+
+        return result; // expose
+    })(['error', 'debug', 'info', 'warn']); // _log
+
+    window.LOGENABLED = true;
+
+    var show_log_buffer = false;
+    $("#log_buffer_dump").before($('<button>toggle full log buffer snapshot</button>').on('click',function(){
+        show_log_buffer = !show_log_buffer;
+        if (show_log_buffer) {
+            $("#log_buffer_dump").html(log_buffer.join(''));
+        } else {
+            $("#log_buffer_dump").html("");
+        }
+    })).on("touchenter",function(){console.log("touchenter on toggle buffer dump button");})
+        .on('touchleave',function(){console.log("touchleave on toggle buffer dump button");});
 
     function error(e) {
         var e_html = '<div class="error">'+e.toString()+" at "+e.stack+"</div>";
@@ -344,11 +411,11 @@ var DEBUG = (function($) {
         datenow: datenow
     };
 
-    // generally helpful debugging info
+    // generally helpful debugging info we're gonna globally provide
     $(function() {
-        console.log("UA: "+navigator.userAgent);
-        console.log("window.devicePixelRatio:", window.devicePixelRatio);
-        console.log("Revision: "+exposed.revision);
+        console.info("UA: "+navigator.userAgent);
+        console.info("window.devicePixelRatio:", window.devicePixelRatio);
+        console.info("Revision: "+exposed.revision);
     });
     return exposed;
 })(window.ply_$ || jQuery);
